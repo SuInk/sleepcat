a0d2774dbc9f59c10b62828a62424c8f04f40065
SuInk <36656552+SuInk@users.noreply.github.com>
2026-09-16 18:26:23 +0800

    Release 1.6.4

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>CFBundleIdentifier</key>      <string>$IDENTIFIER</string>
     <key>CFBundleName</key>            <string>SleepCat</string>
     <key>CFBundleDisplayName</key>     <string>SleepCat</string>
-    <key>CFBundleShortVersionString</key> <string>1.6.3</string>
+    <key>CFBundleShortVersionString</key> <string>1.6.4</string>
     <key>CFBundleVersion</key>         <string>1</string>
     <key>CFBundlePackageType</key>     <string>APPL</string>
     <key>CFBundleIconFile</key>        <string>AppIcon</string>
